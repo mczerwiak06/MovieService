0c75a0cddc85449fa4713e7672bc12ac7b6d5a47
Michał Czerwiak <michal.czerwiak@gmail.com>
2021-06-04 08:06:32 +0200

    query update

--- MovieService/src/main/java/pl/pjatk/zjazd4/MovieService/repository/MovieRepository.java
+++ MovieService/src/main/java/pl/pjatk/zjazd4/MovieService/repository/MovieRepository.java
@@ -1,6 +1,6 @@
 package pl.pjatk.zjazd4.MovieService.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.pjatk.zjazd4.MovieService.model.Movie;
 
 import java.util.List;
@@ -9,14 +9,18 @@ import java.util.Optional;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Override
+    @Query("select m from Movie m")
     List<Movie> findAll();
 
     @Override
+    @Query("select m from Movie m where m.id = ?1")
     Optional<Movie> findById(Long along);
 
     @Override
+    @Query(value = "insert into Movie values (?1, ?2, ?3)", nativeQuery = true)
     <S extends Movie> S save(S s);
 
     @Override
+    @Query("delete from Movie m where m.id = ?1")
     void deleteById(Long aLong);
 }
